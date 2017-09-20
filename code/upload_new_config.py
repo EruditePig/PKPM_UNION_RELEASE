978518ff2cc46ce3c36fddcc370c93ef47375559
EruditePig <wagalimaxida@qq.com>
2017-09-20 21:05:54 +0800

    继续修改提交配置

--- code/upload_new_config.py
+++ code/upload_new_config.py
@@ -16,7 +16,16 @@ class upload_new_config:
         return g_render.upload_new_config()
 
     def POST(self):
-        pass
+        data = web.data()
+        parsed_data = urlparse.parse_qs(data)
+        request_type = parsed_data.get('action')[0]
+        if request_type == 'request_config':
+            parent_version_id = parsed_data.get('parent_version_id')[0]
+            db = dbwrapper.DBWrapper(dbpath)
+            config_info = db.get_version_config_info(parent_version_id)
+            return json.dumps({"state" : "success", "config_info" : config_info})
+        else:
+            return json.dumps({"state" : "fail", "info" : "unknown request"})
 
 if __name__=="__main__":
 
