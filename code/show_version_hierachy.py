3d6441555166b74f8a53ece6b0a4f4c69a2d6006
EruditePig <wagalimaxida@qq.com>
2017-09-27 23:55:49 +0800

    初步纳入FileAPI

--- code/show_version_hierachy.py
+++ code/show_version_hierachy.py
@@ -14,12 +14,16 @@ class show_version_hierachy:
 
     def POST(self):
         data = web.data()
+        print web.input()
         parsed_data = urlparse.parse_qs(data)
-        request_type = parsed_data.get('action')[0]
-        if request_type == 'request_version_hierachy':
-            db = dbwrapper.DBWrapper(dbpath)
-            version_hierachy = db.get_version_hierachy()
-            return json.dumps({"state" : "success", "version_hierachy" : version_hierachy})
+        if parsed_data:
+            request_type = parsed_data.get('action')[0]
+            if request_type == 'request_version_hierachy':
+                db = dbwrapper.DBWrapper(dbpath)
+                version_hierachy = db.get_version_hierachy()
+                return json.dumps({"state" : "success", "version_hierachy" : version_hierachy})
+        else:
+            print data
 
 if __name__=="__main__":
     pass
